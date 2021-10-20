f492261bedf9c2cfa2da02b1b30109eeed25cede
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-10-20 22:31:50 +0300

    added selectors

--- src/components/redux/contacts-store.js
+++ src/components/redux/contacts-store.js
@@ -1,35 +1,9 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts-reduser';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
-const contactsPersistConfig = {
-  key: 'items',
-  storage,
-  blacklist: ['filter'],
-};
-
-const store = configureStore({
+export const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+    contacts: contactsReducer,
   },
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
   devTools: process.env.NODE_ENV === 'development',
 });
-
-const persistor = persistStore(store);
-
-export default { store, persistor };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import store from './components/redux/contacts-store';
+import { store } from './components/redux/contacts-store';
 import './index.css';
 import { App } from './components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store.store}>
-      <PersistGate loading={null} persistor={store.persistor}>
-        <App />
-      </PersistGate>
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
